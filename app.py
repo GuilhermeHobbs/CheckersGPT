fbdb2dac2f1e602c81ac7f5b988678971be70d02
GuilhermeHobbs <guilhermehobbs@hotmail.com>
2025-02-13 16:01:23 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,24 @@
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app)  # Allow all domains
 
 @app.route('/message', methods=['POST'])
 def message():
     data = request.get_json()
     response = jsonify({"reply": "It's me" if data.get("message") == "Name?" else "Unknown request"})
-    response.headers.add("Access-Control-Allow-Origin", "*")  # Allow CORS for all
+    
+    # Explicitly set CORS headers
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add("Access-Control-Allow-Headers", "Content-Type,Authorization")
+    response.headers.add("Access-Control-Allow-Methods", "POST,OPTIONS")
+
     return response
+
+@app.route('/', methods=['GET'])
+def home():
+    return "Flask API is running!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
